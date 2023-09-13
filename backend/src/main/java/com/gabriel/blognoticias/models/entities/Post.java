4b3525c2baa60f80fe91a8ad5551560813a3c5e3
GabeOP <gabrieloliveirap1@hotmail.com>
2023-09-13 13:41:24 -0300

    entidade post

---
+++ backend/src/main/java/com/gabriel/blognoticias/models/entities/Post.java
@@ -0,0 +1,65 @@
+package com.gabriel.blognoticias.models.entities;
+
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+public class Post {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private UUID id;
+  private String titulo;
+  private String conteudo;
+  private String linkImagem;
+
+  @OneToOne
+  private Usuario autor;
+
+  public Post() {}
+
+  public Post(UUID id, String titulo, String conteudo, String linkImagem, Usuario autor) {
+    this.id = id;
+    this.titulo = titulo;
+    this.conteudo = conteudo;
+    this.linkImagem = linkImagem;
+    this.autor = autor;
+  }
+
+  public UUID getId() {
+    return id;
+  }
+
+  public void setId(UUID id) {
+    this.id = id;
+  }
+
+  public String getTitulo() {
+    return titulo;
+  }
+
+  public void setTitulo(String titulo) {
+    this.titulo = titulo;
+  }
+
+  public String getConteudo() {
+    return conteudo;
+  }
+
+  public void setConteudo(String conteudo) {
+    this.conteudo = conteudo;
+  }
+
+  public String getLinkImagem() {
+    return linkImagem;
+  }
+
+  public void setLinkImagem(String linkImagem) {
+    this.linkImagem = linkImagem;
+  }
+
+  public Usuario getAutor() {
+    return autor;
+  }
+}
