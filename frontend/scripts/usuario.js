5b206d698ae5ea3856404cfdf859a6ffb8b908f5
GabeOP <gabrieloliveirap1@hotmail.com>
2023-09-16 12:18:18 -0300

    redireciona para pagina de cadastro

--- frontend/scripts/usuario.js
+++ frontend/scripts/usuario.js
@@ -1,10 +1,12 @@
 const nomeUsuario = document.getElementById("nomeUsuario");
-
+if(!sessionStorage.getItem("nome")) {
+    window.location.href = "../pages/cadastro.html"
+}
 fetch("http://localhost:8080/usuario")
 .then(x => x.json())
 .then(res => {
 
-    console.log(res)
+
     sessionStorage.setItem("nome", res[0].nome);
 
     nomeUsuario.innerHTML = sessionStorage.getItem("nome");
