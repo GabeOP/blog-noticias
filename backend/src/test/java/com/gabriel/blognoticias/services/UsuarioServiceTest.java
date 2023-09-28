4937f1c97e00bf11e817590e8c37da00ffe03d1f
GabeOP <gabrieloliveirap1@hotmail.com>
2023-09-28 12:09:42 -0300

    testes exceptions e criaUsuario

--- backend/src/test/java/com/gabriel/blognoticias/services/UsuarioServiceTest.java
+++ backend/src/test/java/com/gabriel/blognoticias/services/UsuarioServiceTest.java
@@ -4,6 +4,8 @@ import com.gabriel.blognoticias.enums.Cargo;
 import com.gabriel.blognoticias.models.dto.UsuarioDTO;
 import com.gabriel.blognoticias.models.dto.UsuarioResponseDTO;
 import com.gabriel.blognoticias.models.entities.Usuario;
+import com.gabriel.blognoticias.models.exception.JaCadastradoException;
+import com.gabriel.blognoticias.models.exception.NaoEncontradoException;
 import com.gabriel.blognoticias.repositories.UsuarioRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -18,6 +20,7 @@ import java.util.List;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
@@ -70,6 +73,38 @@ public class UsuarioServiceTest {
     assertEquals(response.getClass(), UsuarioDTO.class);
     assertEquals(response.getNome(), NOME);
     assertEquals(response.getCargo(), Cargo.COMUM);
+  }
+
+  @Test
+  void whenFindByNomeThenThrowNaoEncontradoException() {
+    when(repository.findByNome(any())).thenReturn(null);
+
+    assertThrows(NaoEncontradoException.class, () -> {
+      service.findByNome(NOME);
+    });
+  }
+
+  @Test
+  void whenCriaUsuarioThenCreateANewUser() {
+    when(repository.save(any())).thenReturn(usuario);
+
+     String response = service.criaUsuario(usuarioDTO);
+
+     assertEquals(response.getClass(), String.class);
+    System.out.println(response);
+  }
+
+  @Test
+  void whenCriaUsuarioThenThrowJaCadastradoException() {
+    when(repository.save(any()))
+            .thenThrow(new JaCadastradoException("Usuário já cadastrado."));
+
+    try{
+      repository.save(any());
+    }catch(Exception ex) {
+      assertEquals(JaCadastradoException.class, ex.getClass());
+      assertEquals("Usuário já cadastrado.", ex.getMessage());
+    }
   }
 
 
