0e1d808f81615036756d67e5db1b53151dda2309
GabeOP <gabrieloliveirap1@hotmail.com>
2023-12-14 13:33:42 -0300

    comentario funcional

--- frontend/scripts/postagem.js
+++ frontend/scripts/postagem.js
@@ -7,7 +7,7 @@ const queryString = window.location.search;
 const params = new URLSearchParams(queryString);
 
 // Obtém o valor do parâmetro 'id'
-const id = params.get('id');
+const postagem_id = params.get('id');
 
 //Você está conectado como: 
 nomeUsuario.innerHTML = sessionStorage.getItem("nome");
@@ -15,7 +15,7 @@ nomeUsuario.innerHTML = sessionStorage.getItem("nome");
 const container = document.getElementById("container");
 const wrapPost = document.getElementById("wrapPost");
 
-fetch("http://localhost:8080/post/" + id, {
+fetch("http://localhost:8080/post/" + postagem_id, {
     method: "GET",
     headers: {
         "Authorization": "Bearer " + sessionStorage.getItem("token")
@@ -48,26 +48,37 @@ fetch("http://localhost:8080/post/" + id, {
     conteudo.id = "conteudo";
     conteudo.innerHTML = res.conteudo;
 
+    const listaComentario = document.getElementById("listaComentario");
     //Parte da div que cria a área dos comentários
-    const divComentario = document.createElement("div");
-    divComentario.id = "divListaComentario";
-
     res.comentarioList.forEach((item, index) => {
-        console.log(item);
+
+        const wrapComentario = document.createElement("div");
+        wrapComentario.id = "wrapComentario";
+    
+        const autorComentario = document.createElement("p");
+        autorComentario.id = "autorComentario";
+        autorComentario.innerHTML = item.autorComentario.nome;
+
+        const textoComentario = document.createElement("p");
+        textoComentario.id = "textoComentario";
+        textoComentario.innerHTML = item.comentario;
+    
+        const dataComentario = document.createElement("p");
+        dataComentario.id = "dataComentario";
+        dataComentario.innerHTML = item.dataComentario;
+
+            
+        wrapComentario.appendChild(autorComentario);
+        wrapComentario.appendChild(textoComentario);
+        wrapComentario.appendChild(dataComentario);
+            
+        listaComentario.appendChild(wrapComentario);
+        container.appendChild(divComentario);
     })
-
-    const autorComentario = document.createElement("p");
-    autorComentario.id = "autorComentario";
-
-    const textoComentario = document.createElement("p");
-    textoComentario.id = "textoComentario";
-
-    const dataComentario = document.createElement("p");
-    dataComentario.id = "dataComentario";
 
     container.appendChild(divTitulo);
     container.appendChild(divMain);
-    container.appendChild(divComentario);
+
 
     divTitulo.appendChild(titulo);
     divTitulo.appendChild(autor);
@@ -77,8 +88,5 @@ fetch("http://localhost:8080/post/" + id, {
 
     wrapPost.appendChild(divTitulo);
     wrapPost.appendChild(divMain);
-    
-    divComentario.appendChild(autorComentario);
-    divComentario.appendChild(textoComentario);
-    divComentario.appendChild(dataComentario);
-})
+
+});
