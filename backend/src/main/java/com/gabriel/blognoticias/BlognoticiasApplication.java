cdd2c30406fc445b433ce942f94fb8b61925e068
GabeOP <gabrieloliveirap1@hotmail.com>
2023-12-30 19:43:06 -0300

    scheduled teste

--- backend/src/main/java/com/gabriel/blognoticias/BlognoticiasApplication.java
+++ backend/src/main/java/com/gabriel/blognoticias/BlognoticiasApplication.java
@@ -2,12 +2,19 @@ package com.gabriel.blognoticias;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
+@EnableScheduling
 public class BlognoticiasApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlognoticiasApplication.class, args);
 	}
 
+	@Scheduled(fixedDelay = 14000)
+	private static void antiafk() {
+		System.out.println(".");
+	}
 }
